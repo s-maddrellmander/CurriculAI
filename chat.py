ad1166c0cb919f2516cd4fdb6e326db755984cda
Sam Maddrell-Mander <samuelm@graphcore.ai>
2023-06-28 14:45:25 +0100

    Basic sweep over directory of directories - issue with special tokens

--- chat.py
+++ chat.py
@@ -1,4 +1,12 @@
+import glob
 import os
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import Chroma
 
 os.environ["OPENAI_API_KEY"] = "YOUR API KEY"
 
@@ -6,7 +14,26 @@ from langchain.document_loaders import PyPDFDirectoryLoader
 
 
 def chat():
-    pdf_folder_path = "~/Zotero/storage/*"
-    loader = PyPDFDirectoryLoader(pdf_folder_path)
+    pdf_folder_path = os.path.expanduser("~/Zotero/storage/")
+    loader = PyPDFDirectoryLoader(
+        pdf_folder_path, glob="*.pdf", recursive=True, silent_errors=True
+    )
     docs = loader.load()
     print(len(docs))
+
+    embeddings = OpenAIEmbeddings()
+    vectordb = Chroma.from_documents(
+        docs,
+        embedding=embeddings,
+        persist_directory="chroma_db/",
+        disallowed_special=(),
+    )
+    vectordb.persist()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    pdf_qa = ConversationalRetrievalChain.from_llm(
+        OpenAI(temperature=0.8), vectordb.as_retriever(), memory=memory
+    )
+    query = "Provide a summary of what Graph Neural Networks are used for?"
+    result = pdf_qa({"question": query})
+    print("Answer:")
+    result["answer"]
--- pre-commit.sh
+++ pre-commit.sh
@@ -1,3 +1,4 @@
+export PYTHONPATH=:~/Projects/CurriculAI
 isort --profile black *.py tests/*
 black .
 pytest --cov=. --cov-report term --cov-report html
